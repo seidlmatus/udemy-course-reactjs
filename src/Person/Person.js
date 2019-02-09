073ee4b52c41956d63cc54ac5a3ea720ce3b6537
MatusSeidl <seidl.matus@gmail.com>
2019-02-09 14:11:28 +0100

    Understanding the Children

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const Person = (props)=>{
-    return <p>I'm a {props.name}! and I'm {props.age} years old :)</p>
+    return <div><p>I'm a {props.name}! and I'm {props.age} years old :)</p>
+        <p>{props.children}</p>
+    </div>
 }
 
 export default Person;
