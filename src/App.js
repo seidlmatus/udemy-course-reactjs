aee4ba703ade35270b5a5eab4a189241b4391c9f
MatusSeidl <seidl.matus@gmail.com>
2019-02-09 20:24:52 +0100

    Using the useState() Hook for State Manipulation

--- src/App.js
+++ src/App.js
@@ -1,42 +1,45 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
+const app = props => {
 
-  state = {
-    persons: [
-      { name: 'Matus', age: '25' },
-      { name: 'John', age: '17' },
-      { name: 'Emma', age: '21' }
-    ],
-    otherState:'some other value'
-  }
-
-  switchNameHandler = () => {
-    //console.log('Was clicked !');
-    //DON'T DO THIS:  this.state.persons[0].name = 'Igor';
-    this.setState({
+   const [personState, setPersonsState] = useState({
       persons: [
-        { name: 'Emma', age: '25' },
-        { name: 'Matus', age: '17' },
-        { name: 'John', age: '30' }
-      ]
+        { name: 'Matus', age: '25' },
+        { name: 'John', age: '17' },
+        { name: 'Emma', age: '21' }
+      ],
+      otherState:'some other value'
     });
-  }
 
-  render() {
+    console.log(personState);
+
+   const switchNameHandler = () => {
+      //console.log('Was clicked !');
+      //DON'T DO THIS:  this.state.persons[0].name = 'Igor';
+      setPersonsState({
+        persons: [
+          { name: 'Emma', age: '25' },
+          { name: 'Matus', age: '17' },
+          { name: 'John', age: '30' }
+        ]
+      });
+    }
+
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
         <p>This is working...</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Coding</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personState.persons[0].name} age={personState.persons[0].age} />
+        <Person name={personState.persons[1].name} age={personState.persons[1].age} >My Hobbies: Coding</Person>
+        <Person name={personState.persons[2].name} age={personState.persons[2].age} />
       </div>
     );
   }
-}
 
-export default App;
+export default app;
+
+
+
