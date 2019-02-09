f5d6b7dc67e4e6ee3b8ecd7d21084244565b72aa
MatusSeidl <seidl.matus@gmail.com>
2019-02-09 21:51:21 +0100

    Handling Dynamic Content "The JavaScript Way"

--- src/App.js
+++ src/App.js
@@ -51,27 +51,32 @@ class App extends Component {
       couser:'pointer'
     }
 
+
+    let persons = null;
+
+    if(this.state.showPersons){
+      persons=(
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age} />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age} 
+            click={this.switchNameHandler.bind(this,'Max')}
+            changed={this.nameChangeHandler}>My Hobbies: Coding</Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age} />
+        </div>
+      );
+    }
     return (
       <div className="App">
         <h1>Hi, I am a React App</h1>
         <p>This is working...</p>
-        <button style={styleBtn} onClick={this.togglePersonsHandler}>Show Names!</button>
-        {
-          this.state.showPersons === true?  
-            <div>
-            <Person
-              name={this.state.persons[0].name}
-              age={this.state.persons[0].age} />
-            <Person
-              name={this.state.persons[1].name}
-              age={this.state.persons[1].age} 
-              click={this.switchNameHandler.bind(this,'Max')}
-              changed={this.nameChangeHandler}>My Hobbies: Coding</Person>
-            <Person
-              name={this.state.persons[2].name}
-              age={this.state.persons[2].age} />
-              </div>: null
-        }
+        <button style={styleBtn} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
       </div>
     );
   }
